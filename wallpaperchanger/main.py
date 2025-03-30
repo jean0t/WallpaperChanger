293d82fc9ad40e5e0c382bc3c1381cf10ae3c0c4
jean0t <joaomouryf@hotmail.com>
2025-03-30 11:12:27 -0300

    main.py updated with desktop environments and main function

--- wallpaperchanger/main.py
+++ wallpaperchanger/main.py
@@ -1,10 +1,16 @@
 from wallpaperchanger.desktop_environments.kde import KdeEnvironment
 from wallpaperchanger.desktop_environments.gnome import GnomeEnvironment
+from wallpaperchanger.desktop_environments.xfce import XfceEnvironment
+from wallpaperchanger.desktop_environments.lxqt import LxqtEnvironment
+from wallpaperchanger.desktop_environments.cinnamon import CinnamonEnvironment
+from wallpaperchanger.desktop_environments.sway import SwayEnvironment
+from wallpaperchanger.desktop_environments.hyprland import HyprlandEnvironment
 from wallpaperchanger.desktop_environments.unknown import UnknownEnvironment
 from wallpaperchanger.discover_de import Discover
 from pathlib import PosixPath
 
 
+# To Do: Not all desktop environments are Implemented yet
 DESKTOP_ENVIRONMENT_MAP = {
 # kde
 "kde": KdeEnvironment,
@@ -21,7 +27,16 @@ DESKTOP_ENVIRONMENT_MAP = {
 
 # cinnamon
 "cinnamon": CinnamonEnvironment,
-"x-cinnamon": CinnamonEnvironment
+"x-cinnamon": CinnamonEnvironment,
+
+# sway
+"sway": SwayEnvironment,
+
+# hyprland
+"hyprland": HyprlandEnvironment,
+
+# i3
+"i3": I3Environment
 }
 
 
@@ -30,3 +45,4 @@ def main(wallpaper_path: PosixPath) -> None:
 	changer = DESKTOP_ENVIRONMENT_MAP.get(desktop, UnknownEnvironment)()
 
 	# to_do: complete the main function
+	pass
