229e2e32218fc014eda3562f6b204cee8db8573d
jean0t <joaomouryf@hotmail.com>
2025-03-30 14:28:28 -0300

    refactor: error.py had comments to better visualize the messages

--- wallpaperchanger/desktop_environments/error.py
+++ wallpaperchanger/desktop_environments/error.py
@@ -1,3 +1,7 @@
 class Error:
+	# error to set wallpaper
 	FailedToSetWallpaperKde = "Failed to set Wallpaper, have you installed qdbus?"
+	FailedToSetWallpaperCinnamon = "Failed to set Wallpaper, is the Path correct?"
+
+	# unknown desktop environment
 	UnknownDesktopSetWallpaper = 'Unknown Desktop Environment, unable to identify and set wallpaper.'
