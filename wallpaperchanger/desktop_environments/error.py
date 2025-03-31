fe39e3de0205655084c1e13defa738c09982389f
jean0t <joaomouryf@hotmail.com>
2025-03-30 22:00:05 -0300

    xfce implemented

--- wallpaperchanger/desktop_environments/error.py
+++ wallpaperchanger/desktop_environments/error.py
@@ -3,6 +3,7 @@ class Error:
 	FailedToSetWallpaperKde = "Failed to set Wallpaper, have you installed qdbus?"
 	FailedToSetWallpaperCinnamon = "Failed to set Wallpaper, is the Path correct?"
 	FailedToSetWallpaperGnome = "Failed to set Wallpaper, is the Path correct?"
+	FailedToSetWallpaperXfce = "Failed to set Wallpaper, is the Path correct?"
 
 	# unknown desktop environment
 	UnknownDesktopSetWallpaper = 'Unknown Desktop Environment, unable to identify and set wallpaper.'
--- wallpaperchanger/desktop_environments/xfce.py
+++ wallpaperchanger/desktop_environments/xfce.py
@@ -1,5 +1,11 @@
 from wallpaperchanger.desktop_environments.base import DesktopEnvironment
 from subprocess import run, CalledProcessError
+from pathlib import PosixPath
+from wallpaperchanger.desktop_environments.error import Error
 
 class XfceEnvironment(DesktopEnvironment):
-	pass
+	def set_wallpaper(path: PosixPath): # must be absolute path
+		try:
+			run(["xfconf-query", "--channel", "xfce4-desktop", "--property", "/backdrop/screen0/monitor0/image-path", "--set", path.__str__()], check=True)
+		except CalledProcessError:
+			print(Error.FailedToSetWallpaperXfce)
