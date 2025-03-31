7a1549384cbc6e653252a4e1b1918bcd56185a22
jean0t <joaomouryf@hotmail.com>
2025-03-30 21:55:49 -0300

    gnome environment added

--- wallpaperchanger/desktop_environments/gnome.py
+++ wallpaperchanger/desktop_environments/gnome.py
@@ -1,5 +1,12 @@
 from wallpaperchanger.desktop_environments.base import DesktopEnvironment
 from subprocess import run, CalledProcessError
+from pathlib import PosixPath
+from wallpaperchanger.desktop_environments.error import Error
+
 
 class GnomeEnvironment(DesktopEnvironment):
-	pass
+	def set_wallpaper(path: PosixPath): # must be absolute path
+		try:
+			run(["gsettings", "set", "org.gnome.desktop.background", "picture-uri", f"file://{path.__str__()}"], check=True)
+		except CalledProcessError:
+			print(Error.FailedToSetWallpaperGnome)
