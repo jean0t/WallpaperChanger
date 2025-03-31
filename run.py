6021328e636c8fd03a446b0f7d79a1962fbc873c
jean0t <joaomouryf@hotmail.com>
2025-03-30 22:42:04 -0300

    run.py finished

--- run.py
+++ run.py
@@ -1,2 +1,16 @@
+from wallpaperchanger.main import main as execute
+from pathlib import Path
+from argparse import ArgumentParser
+
 def main():
-	pass
+	parser = ArgumentParser(description="Changes the wallpaper")
+	parser.add_argument("wallpaper_path", type=Path, help="Path to the wallpaper")
+	args = parser.parse_args()
+
+	if args.wallpaper_path.exists():
+		execute(args.wallpaper_path.absolute())
+	else:
+		exit(1)
+
+if __name__ == "__main__":
+	main()
