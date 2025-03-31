417dee5a6041464912935bbb38098409aadf656f
jean0t <joaomouryf@hotmail.com>
2025-03-30 22:57:59 -0300

    hyprland implementation added

--- wallpaperchanger/desktop_environments/error.py
+++ wallpaperchanger/desktop_environments/error.py
@@ -1,12 +1,21 @@
 class Error:
 	# error to set wallpaper
+
+	# qt
 	FailedToSetWallpaperKde = "Failed to set Wallpaper, have you installed qdbus?"
+	
+	# gtk
 	FailedToSetWallpaperCinnamon = "Failed to set Wallpaper, is the Path correct?"
 	FailedToSetWallpaperGnome = "Failed to set Wallpaper, is the Path correct?"
+	FailedToSetWallpaperLxqt = "Failed to set Wallpaper, is the Path correct?"
+
+	# xorg
 	FailedToSetWallpaperXfce = "Failed to set Wallpaper, is the Path correct?"
 	FailedToSetWallpaperI3 = "Failed to set Wallpaper, is the Path correct?"
-	FailedToSetWallpaperSway = "Failed to set Wallpaper, is the Path correct?"
-	FailedToSetWallpaperLxqt = "Failed to set Wallpaper, is the Path correct?"
+	
+	# wayland
+	FailedToSetWallpaperSway = "Failed to set Wallpaper, have you installed swaybg?"
+	FailedToSetWallpaperhyprland = "Failed to set Wallpaper, have you installed hyprpaper?"
 
 	# unknown desktop environment
 	UnknownDesktopSetWallpaper = 'Unknown Desktop Environment, unable to identify and set wallpaper.'
--- wallpaperchanger/desktop_environments/hyprland.py
+++ wallpaperchanger/desktop_environments/hyprland.py
@@ -1,5 +1,12 @@
 from wallpaperchanger.desktop_environments.base import DesktopEnvironment
 from subprocess import run, CalledProcessError
+from pathlib import PosixPath
+from wallpaperchanger.desktop_environments.error import Error
 
 class HyprlandEnvironment(DesktopEnvironment):
-	pass
+	def set_wallpaper(self, path: PosixPath): # must be an absolute path
+		try:
+			run(["hyprpaper", path.__str__()], check=True)
+
+		except CalledProcessError:
+			print(Error.FailedToSetWallpaperHyprland)
